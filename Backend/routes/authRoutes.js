6bfb9c32d920ad2f23a551b6d7b8e0bfabe0cbf5
Ashish <128583121+ash0github@users.noreply.github.com>
2025-10-05 20:53:21 +0200

    0.2.3 - Configured MFA
    Added routes for verifyOTP. Installed packages for nodemailer services.
    Created an OTP model.
    Added in middleware to assemble and send out mail from an an address with injected env credentials.
    Configured authController with a sendMail method that uses the middleware. Additionally, added in the export for OTP verification.
    
    Notes:
    OTPs are hashed so it is secure in the database.
    OTP's are deleted on use and no more than one per user can exist.
    sendMail is called from the Login exported route when a valid user signs in.

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const {register, login} = require('../controllers/authController');
+const {register, login, verifyOTP} = require('../controllers/authController');
 const {protect} = require('../services/authMiddleware');
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/verifyOTP", verifyOTP);
 
 router.get("/protected", protect, (req, res) => {
     res.json({
