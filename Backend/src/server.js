431065f3fdb39f3eec7ee1659dc8e26ec8b48ab7
Ashish <128583121+ash0github@users.noreply.github.com>
2025-10-09 23:08:25 +0200

    Update server.js
    added mongo connection for render logic

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
@@ -11,26 +12,32 @@ const PORT = process.env.PORT || 5050;
 
 const isRender = process.env.RENDER === 'true'
 
-if (isRender) {
-  // use plain HTTP for Render
-  app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running (HTTP) on port ${PORT}`);
-  });
+if (isRender) 
+{
+    mongoose.connect(process.env.MONGO_CONNECTION)
+    .then(() => {
+        http.createServer(app).listen(PORT, () => {
+        console.log(`Server running on render at ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('MongoDb connection failed. Error:', err);
+    });
 } 
 else 
 {
     const options = {
-    key: fs.readFileSync(path.join(__dirname, '../certs/server.key')),
-    cert: fs.readFileSync(path.join(__dirname, '../certs/server.crt')),
-};
+        key: fs.readFileSync(path.join(__dirname, '../certs/server.key')),
+        cert: fs.readFileSync(path.join(__dirname, '../certs/server.crt')),
+    };
 
-mongoose.connect(process.env.MONGO_CONNECTION)
-.then(() => {
-    https.createServer(options, app).listen(PORT, () => {
-    console.log(`Server running at https://localhost:${PORT}`);
+    mongoose.connect(process.env.MONGO_CONNECTION)
+    .then(() => {
+        https.createServer(options, app).listen(PORT, () => {
+        console.log(`Server running at https://localhost:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('MongoDb connection failed. Error:', err);
     });
-})
-.catch((err) => {
-    console.error('MongoDb connection failed. Error:', err);
-});
 }
