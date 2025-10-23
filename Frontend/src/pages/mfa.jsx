fe0aa92d54e5bd94bdd7c3a1f5a42620591e472c
Fathima Shariff <128419928+st10371629@users.noreply.github.com>
2025-10-23 19:08:37 +0200

    mfa changes
    - able to paste and backspace codes now
    - styling adjustments

--- Frontend/src/pages/mfa.jsx
+++ Frontend/src/pages/mfa.jsx
@@ -20,6 +20,29 @@ const MFA = () => {
     }
   };
 
+  // Handle continuous backspace
+  const handleKeyDown = (e, index) => {
+    if (e.key === 'Backspace' && !code[index] && index > 0) {
+      // If backspace is pressed on an empty field, focus the previous field
+      document.getElementById(`code-${index - 1}`).focus();
+    }
+  };
+
+  // Handle pasting the code
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const pastedData = e.clipboardData.getData('text');
+    
+    // Check if pasted data is a 6-digit number
+    if (pastedData.length === 6 && /^[0-9]{6}$/.test(pastedData)) {
+      const newCode = pastedData.split('');
+      setCode(newCode);
+      
+      // Focus the last input after paste
+      document.getElementById('code-5').focus();
+    }
+  };
+
   const handleSubmit = async () => {
   const fullCode = code.join('');
   const email = localStorage.getItem("email");
@@ -55,29 +78,35 @@ const MFA = () => {
             </header>
 
       <main className="mfa-main">
-        <div className="mfa-card">
-            <h2>Multi-Factor Authentication</h2>
-            <p>Please enter the code sent to your email to proceed.</p>
-        </div>
 
-        {/* Input Fields */}
-        <div className="mfa-input-group">
-        {code.map((digit, index) => (
-            <input
-            key={index}
-            id={`code-${index}`}
-            type="text"
-            maxLength="1"
-            className="mfa-input"
-            value={digit}
-            onChange={(e) => handleChange(e.target.value, index)}
-            />
-        ))}
-        </div>
+        <div className="mfa-outline-card">
+          <div className="mfa-card">
+              <h2>Multi-Factor Authentication</h2>
+              <p>Please enter the code sent to your email to proceed.</p>
+          </div>
 
-        <button className="mfa-submit" onClick={handleSubmit}>
-          Submit
-        </button>
+          {/* Input Fields */}
+          <div className="mfa-input-group" onPaste={handlePaste}>
+          {code.map((digit, index) => (
+              <input
+              key={index}
+              id={`code-${index}`}
+              type="tel"
+              inputMode="numeric" // for browser to show numeric pad
+              autoComplete="one-time-code"
+              maxLength="1"
+              className="mfa-input"
+              value={digit}
+              onChange={(e) => handleChange(e.target.value, index)}
+              onKeyDown={(e) => handleKeyDown(e, index)}
+              />
+          ))}
+          </div>
+
+          <button className="mfa-submit" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
       </main>
     </div>
   );
